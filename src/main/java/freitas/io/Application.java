87a73f3d2a4c70a04ca614d6414f0876d3ae7d7c
EdsonSFreitas <edson.s.freitas@gmail.com>
2023-10-04 13:39:46 -0300

    Ajuste de CORS e Deploy da aplicacao no Railway

--- src/main/java/freitas/io/Application.java
+++ src/main/java/freitas/io/Application.java
@@ -1,8 +1,11 @@
 package freitas.io;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(servers = @Server(url = "/", description = "Default Server URL for API"))
 @SpringBootApplication
 public class Application {
 
