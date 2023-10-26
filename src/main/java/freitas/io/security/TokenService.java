138f41b04e24086edcbceefffe0c5a5a9c76e75f
EdsonSFreitas <edson.s.freitas@gmail.com>
2023-10-26 15:50:50 -0300

    Token Expiration e Token Secret migrado para o application.yml

--- src/main/java/freitas/io/security/TokenService.java
+++ src/main/java/freitas/io/security/TokenService.java
@@ -3,6 +3,7 @@ package freitas.io.security;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import freitas.io.domain.model.User;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
@@ -16,7 +17,13 @@ import java.util.Date;
  */
 @Service
 public class TokenService {
-    //String secret = System.getenv("secret");
+
+    @Value("${api.security.token.secret}")
+    private String secret;
+
+    @Value("${api.security.token.jwt.expiration_in_minutes}")
+    private int expirationInMinutes;
+
 
     public String generateToken(User user) {
         return JWT.create()
@@ -24,14 +31,13 @@ public class TokenService {
                 .withSubject(user.getUsername())
                 .withClaim("id", user.getId().toString())
                 .withExpiresAt(Date.from(LocalDateTime.now()
-                        .plusMinutes(20)
+                        .plusMinutes(expirationInMinutes)
                         .toInstant(ZoneOffset.of("-03:00"))))
-                .sign(Algorithm.HMAC256("freitas.io"));
-        //TODO Salvar secret como variavel de ambiente ao inves de usar como hardcoded
+                .sign(Algorithm.HMAC256(secret));
     }
 
     public String getSubject(String token) {
-        return JWT.require(Algorithm.HMAC256("freitas.io"))
+        return JWT.require(Algorithm.HMAC256(secret))
                 .withIssuer("freitas.io")
                 .build()
                 .verify(token).getSubject();
